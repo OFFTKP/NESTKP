4b7c1efd6887cdd0a171a31003234a15f819a073
OFFTKP <parisoplop@gmail.com>
2021-12-26 22:58:55 +0200

    cpu additions

--- nes_cpu.cpp
+++ nes_cpu.cpp
@@ -1,4 +1,22 @@
 #include "nes_cpu.h"
 namespace TKPEmu::NES::Devices {
-    
+    TickRet NES::tick() {
+        // Instruction queue splits the instruction to necessary parts
+        // so that later NES update function will run PPU, APU and other updates
+        // in the middle of the instruction
+        instruction_queue_.push([](){
+            // Instruction fetching time
+            return static_cast<FuncRet>(1);
+        });
+        uint8_t opcode = read(PC);
+        const Instruction& instr = instructions_[opcode];
+        // Handle addressing mode
+        instruction_queue_.push(get_addr_mode(instr.addr));
+    }
+
+    FuncRetPtr NES::get_addr_mode(AddressingMode addr) {
+        switch (addr) {
+            
+        }
+    }
 }
--- nes_cpu.h
+++ nes_cpu.h
@@ -8,6 +8,9 @@
 #include <queue>
 #include <string>
 namespace TKPEmu::NES::Devices {
+    using FuncRet = void;
+    using FuncRetPtr = FuncRet (*)();
+    using TickRet = bool;
     namespace {
         union FlagUnion {
         public:
@@ -34,13 +37,13 @@ namespace TKPEmu::NES::Devices {
             CLD, CLI, CLV, CMP, CPX, CPY, DEC, DEX, DEY, EOR, INC, INX, INY, JMP,
             JSR, LDA, LDX, LDY, LSR, NOP, ORA, PHA, PHP, PLA, PLP, ROL, ROR, RTI,
             RTS, SBC, SEC, SED, SEI, STA, STX, STY, TAX, TAY, TSX, TXA, TXS, TYA,
-            XXO
+            XXX
         };
         enum AddressingMode {
-            IMP, IMM, ZPG, ZPX,
+            IMP, IMM, ZP0, ZPX,
             ZPY, ABS, ABX, ABY,
-            IND, IDX, IDY, ACC,
-            REL, XXA
+            IND, IZX, IZY, ACC,
+            REL
         };
         struct Instruction {
             Operation op;
@@ -52,23 +55,37 @@ namespace TKPEmu::NES::Devices {
     private:
         std::array<Instruction, 0x100> instructions_ = { {
             #define I(A,B) { A, B, #A ", " #B }
-            I(BRK,IMP), I(ORA,IND), I(XXO,XXA), I(XXO,XXA), I(XXO,XXA), I(ORA,ZPG), I(ASL,ZPG), I(XXO,XXA), I(PHP,IMP), I(ORA,IMM), I(ASL,ACC), I(XXO,XXA), I(XXO,XXA), I(ORA,ABS), I(ASL,ABS), I(XXO,XXA),
-            I(BPL,REL), I(ORA,IND), I(XXO,XXA), I(XXO,XXA), I(XXO,XXA), I(ORA,ZPX), I(ASL,ZPX), I(XXO,XXA), I(CLC,IMP), I(ORA,ABY), I(XXO,XXA), I(XXO,XXA), I(XXO,XXA), I(ORA,ABX), I(ASL,ABX), I(XXO,XXA),
-            I(JSR,ABS), I(AND,IND), I(XXO,XXA), I(XXO,XXA), I(BIT,ZPG), I(AND,ZPG), I(ROL,ZPG), I(XXO,XXA), I(PLP,IMP), I(AND,IMM), I(ROL,ACC), I(XXO,XXA), I(BIT,ABS), I(AND,ABS), I(ROL,ABS), I(XXO,XXA),
-            I(BMI,REL), I(AND,IND), I(XXO,XXA), I(XXO,XXA), I(XXO,XXA), I(AND,ZPX), I(ROL,ZPX), I(XXO,XXA), I(SEC,IMP), I(AND,ABY), I(XXO,XXA), I(XXO,XXA), I(XXO,XXA), I(AND,ABX), I(ROL,ABX), I(XXO,XXA),
-
+            I(BRK, IMM), I(ORA, IZX), I(XXX, IMP), I(XXX, IMP), I(NOP, IMP), I(ORA, ZP0), I(ASL, ZP0), I(XXX, IMP), I(PHP, IMP), I(ORA, IMM), I(ASL, IMP), I(XXX, IMP), I(NOP, IMP), I(ORA, ABS), I(ASL, ABS), I(XXX, IMP),
+            I(BPL, REL), I(ORA, IZY), I(XXX, IMP), I(XXX, IMP), I(NOP, IMP), I(ORA, ZPX), I(ASL, ZPX), I(XXX, IMP), I(CLC, IMP), I(ORA, ABY), I(NOP, IMP), I(XXX, IMP), I(NOP, IMP), I(ORA, ABX), I(ASL, ABX), I(XXX, IMP),
+            I(JSR, ABS), I(AND, IZX), I(XXX, IMP), I(XXX, IMP), I(BIT, ZP0), I(AND, ZP0), I(ROL, ZP0), I(XXX, IMP), I(PLP, IMP), I(AND, IMM), I(ROL, IMP), I(XXX, IMP), I(BIT, ABS), I(AND, ABS), I(ROL, ABS), I(XXX, IMP),
+            I(BMI, REL), I(AND, IZY), I(XXX, IMP), I(XXX, IMP), I(NOP, IMP), I(AND, ZPX), I(ROL, ZPX), I(XXX, IMP), I(SEC, IMP), I(AND, ABY), I(NOP, IMP), I(XXX, IMP), I(NOP, IMP), I(AND, ABX), I(ROL, ABX), I(XXX, IMP),
+            I(RTI, IMP), I(EOR, IZX), I(XXX, IMP), I(XXX, IMP), I(NOP, IMP), I(EOR, ZP0), I(LSR, ZP0), I(XXX, IMP), I(PHA, IMP), I(EOR, IMM), I(LSR, IMP), I(XXX, IMP), I(JMP, ABS), I(EOR, ABS), I(LSR, ABS), I(XXX, IMP),
+            I(BVC, REL), I(EOR, IZY), I(XXX, IMP), I(XXX, IMP), I(NOP, IMP), I(EOR, ZPX), I(LSR, ZPX), I(XXX, IMP), I(CLI, IMP), I(EOR, ABY), I(NOP, IMP), I(XXX, IMP), I(NOP, IMP), I(EOR, ABX), I(LSR, ABX), I(XXX, IMP),
+            I(RTS, IMP), I(ADC, IZX), I(XXX, IMP), I(XXX, IMP), I(NOP, IMP), I(ADC, ZP0), I(ROR, ZP0), I(XXX, IMP), I(PLA, IMP), I(ADC, IMM), I(ROR, IMP), I(XXX, IMP), I(JMP, IND), I(ADC, ABS), I(ROR, ABS), I(XXX, IMP),
+            I(BVS, REL), I(ADC, IZY), I(XXX, IMP), I(XXX, IMP), I(NOP, IMP), I(ADC, ZPX), I(ROR, ZPX), I(XXX, IMP), I(SEI, IMP), I(ADC, ABY), I(NOP, IMP), I(XXX, IMP), I(NOP, IMP), I(ADC, ABX), I(ROR, ABX), I(XXX, IMP),
+            I(NOP, IMP), I(STA, IZX), I(NOP, IMP), I(XXX, IMP), I(STY, ZP0), I(STA, ZP0), I(STX, ZP0), I(XXX, IMP), I(DEY, IMP), I(NOP, IMP), I(TXA, IMP), I(XXX, IMP), I(STY, ABS), I(STA, ABS), I(STX, ABS), I(XXX, IMP),
+            I(BCC, REL), I(STA, IZY), I(XXX, IMP), I(XXX, IMP), I(STY, ZPX), I(STA, ZPX), I(STX, ZPY), I(XXX, IMP), I(TYA, IMP), I(STA, ABY), I(TXS, IMP), I(XXX, IMP), I(NOP, IMP), I(STA, ABX), I(XXX, IMP), I(XXX, IMP),
+            I(LDY, IMM), I(LDA, IZX), I(LDX, IMM), I(XXX, IMP), I(LDY, ZP0), I(LDA, ZP0), I(LDX, ZP0), I(XXX, IMP), I(TAY, IMP), I(LDA, IMM), I(TAX, IMP), I(XXX, IMP), I(LDY, ABS), I(LDA, ABS), I(LDX, ABS), I(XXX, IMP),
+            I(BCS, REL), I(LDA, IZY), I(XXX, IMP), I(XXX, IMP), I(LDY, ZPX), I(LDA, ZPX), I(LDX, ZPY), I(XXX, IMP), I(CLV, IMP), I(LDA, ABY), I(TSX, IMP), I(XXX, IMP), I(LDY, ABX), I(LDA, ABX), I(LDX, ABY), I(XXX, IMP),
+            I(CPY, IMM), I(CMP, IZX), I(NOP, IMP), I(XXX, IMP), I(CPY, ZP0), I(CMP, ZP0), I(DEC, ZP0), I(XXX, IMP), I(INY, IMP), I(CMP, IMM), I(DEX, IMP), I(XXX, IMP), I(CPY, ABS), I(CMP, ABS), I(DEC, ABS), I(XXX, IMP),
+            I(BNE, REL), I(CMP, IZY), I(XXX, IMP), I(XXX, IMP), I(NOP, IMP), I(CMP, ZPX), I(DEC, ZPX), I(XXX, IMP), I(CLD, IMP), I(CMP, ABY), I(NOP, IMP), I(XXX, IMP), I(NOP, IMP), I(CMP, ABX), I(DEC, ABX), I(XXX, IMP),
+            I(CPX, IMM), I(SBC, IZX), I(NOP, IMP), I(XXX, IMP), I(CPX, ZP0), I(SBC, ZP0), I(INC, ZP0), I(XXX, IMP), I(INX, IMP), I(SBC, IMM), I(NOP, IMP), I(SBC, IMP), I(CPX, ABS), I(SBC, ABS), I(INC, ABS), I(XXX, IMP),
+            I(BEQ, REL), I(SBC, IZY), I(XXX, IMP), I(XXX, IMP), I(NOP, IMP), I(SBC, ZPX), I(INC, ZPX), I(XXX, IMP), I(SED, IMP), I(SBC, ABY), I(NOP, IMP), I(XXX, IMP), I(NOP, IMP), I(SBC, ABX), I(INC, ABX), I(XXX, IMP),
             #undef I
         } };
     public:
         FlagUnion P;
         uint8_t A, X, Y, SP;
         uint16_t PC;
+        TickRet tick();
     private:
         // We split the instruction into it's parts and update APU&PPU in between instructions
         // like on real hardware
-        std::queue<void (*)()> instruction_queue_;
+        std::queue<FuncRetPtr> instruction_queue_;
+        FuncRetPtr get_addr_mode(AddressingMode addr);
+        FuncRet read(uint16_t address);
         template<typename T>
-        uint8_t inc(T& mem, FlagUnion* P_ptr = nullptr) {
+        FuncRet inc(T& mem, FlagUnion* P_ptr = nullptr) {
             ++mem;
             if (P_ptr) {
                 P_ptr->Flags.Zero = !static_cast<bool>(mem);
@@ -77,7 +94,7 @@ namespace TKPEmu::NES::Devices {
             return 1;
         }
         template<typename T>
-        uint8_t dec(T& mem, FlagUnion* P_ptr = nullptr) {
+        FuncRet dec(T& mem, FlagUnion* P_ptr = nullptr) {
             --mem;
             if (P_ptr) {
                 P_ptr->Flags.Zero = !static_cast<bool>(mem);
